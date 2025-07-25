e76b9a3a97884f4f503c59fb0ff6dd77875085f9
Mudit Kumar <115485952+mudit7011@users.noreply.github.com>
2025-07-25 10:00:57 +0530

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api"
+  baseURL: "https://book-review-platform-kk4l.onrender.com/api"
 });
 
 // Attach JWT token if present
